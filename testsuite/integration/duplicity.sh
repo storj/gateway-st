879ced19baaee3e7c27812f54c6c503ff611df81
Sean Harvey <sean@storj.io>
2021-09-16 13:35:15 +1200

    testsuite: copy over necessary changes from gateway-mt to fix test
    there were changes in duplicity with the CI operating system being
    upgraded. These were fixed in Gateway-MT but not in Gateway-ST.
    
    note that this only copies over the minimum changes needed to get
    it working. We still need to work out the testsuite duplication
    between the two repositories later.
    
    Change-Id: I1eb627bee57529a0fe140dda1b26f6ff56dbe6ed

--- testsuite/integration/duplicity.sh
+++ testsuite/integration/duplicity.sh
@@ -30,16 +30,34 @@ export AWS_ACCESS_KEY_ID=$GATEWAY_0_ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$GATEWAY_0_SECRET_KEY
 export PASSPHRASE="PASSPHRASE"
 
-duplicity -v9 $SRC_DIR s3://$GATEWAY_0_ADDR/duplicity/ --s3-unencrypted-connection
+AWS_ENDPOINT="http://${GATEWAY_0_ADDR}"
 
-duplicity -v9 s3://$GATEWAY_0_ADDR/duplicity/ $DST_DIR --s3-unencrypted-connection
+# duplicity 0.8.x w/ boto3 doesn't seem to create buckets anymore
+aws s3 --endpoint "$AWS_ENDPOINT" mb s3://duplicity
+aws s3 --endpoint "$AWS_ENDPOINT" mb s3://duplicity-multipart
+
+duplicity -v9 --s3-endpoint-url="$AWS_ENDPOINT" "$SRC_DIR" \
+	"boto3+s3://duplicity/" \
+	--s3-unencrypted-connection
+
+duplicity -v9 --s3-endpoint-url="$AWS_ENDPOINT" \
+	"boto3+s3://duplicity/" "$DST_DIR" \
+	--s3-unencrypted-connection
 
 require_equal_files_content "$SRC_DIR/backup-testfile-1MiB"  "$DST_DIR/backup-testfile-1MiB"
 require_equal_files_content "$SRC_DIR/backup-testfile-10MiB" "$DST_DIR/backup-testfile-10MiB"
 
-duplicity -v9 $SRC_DIR s3://$GATEWAY_0_ADDR/duplicity-multipart/ --s3-unencrypted-connection --s3-use-multiprocessing --s3-multipart-max-procs 2 --s3-multipart-chunk-size 2097152
+# use multipart upload
+duplicity -v9 --s3-endpoint-url="$AWS_ENDPOINT" "$SRC_DIR" \
+	"boto3+s3://duplicity-multipart/" \
+	--s3-unencrypted-connection \
+	--s3-use-multiprocessing \
+	--s3-multipart-max-procs 2 \
+	--s3-multipart-chunk-size 2097152
 
-duplicity -v9 s3://$GATEWAY_0_ADDR/duplicity-multipart/ $DST_DIR_MULTIPART --s3-unencrypted-connection
+duplicity -v9 --s3-endpoint-url="$AWS_ENDPOINT" \
+	"boto3+s3://duplicity-multipart/" "$DST_DIR_MULTIPART" \
+	--s3-unencrypted-connection
 
 require_equal_files_content "$SRC_DIR/backup-testfile-1MiB"  "$DST_DIR_MULTIPART/backup-testfile-1MiB"
 require_equal_files_content "$SRC_DIR/backup-testfile-10MiB" "$DST_DIR_MULTIPART/backup-testfile-10MiB"
